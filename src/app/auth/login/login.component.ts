e42df067b9e37b32ebeb7dbc2d426c91aab61413
anton_anton <aremenets1@gmail.com>
2025-05-29 18:35:38 +0500

    1

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {FormsModule, NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  imports: [
+    FormsModule
+  ]
+})
+export class LoginComponent {
+  public onSubmit(formData: NgForm) {
+    const enteredEmail = formData.form.value.email
+    const enteredPassword = formData.form.value.password
+
+    console.log(formData.form)
+  }
+}
